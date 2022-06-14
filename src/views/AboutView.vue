0c8ccd8e30e92337621372f60d7c6567cc4f5a08
Fernando Goulart <fernandogmoura@protonmail.com>
2022-06-14 14:41:27 -0300

    Navbar and tasks

---
+++ src/views/AboutView.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="pa-6">
+    <h1>About us</h1>
+    <p>
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis itaque, molestias error officiis, non omnis officia consequatur nisi veniam esse cupiditate aut recusandae est maiores eligendi rem amet beatae commodi.
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis itaque, molestias error officiis, non omnis officia consequatur nisi veniam esse cupiditate aut recusandae est maiores eligendi rem amet beatae commodi.
+    </p>
+    <p>
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis itaque, molestias error officiis, non omnis officia consequatur nisi veniam esse cupiditate aut recusandae est maiores eligendi rem amet beatae commodi.
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis itaque, molestias error officiis, non omnis officia consequatur nisi veniam esse cupiditate aut recusandae est maiores eligendi rem amet beatae commodi.
+    </p>
+    <p>
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis itaque, molestias error officiis, non omnis officia consequatur nisi veniam esse cupiditate aut recusandae est maiores eligendi rem amet beatae commodi.
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis itaque, molestias error officiis, non omnis officia consequatur nisi veniam esse cupiditate aut recusandae est maiores eligendi rem amet beatae commodi.
+    </p>
+    <p>
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis itaque, molestias error officiis, non omnis officia consequatur nisi veniam esse cupiditate aut recusandae est maiores eligendi rem amet beatae commodi.
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis itaque, molestias error officiis, non omnis officia consequatur nisi veniam esse cupiditate aut recusandae est maiores eligendi rem amet beatae commodi.
+    </p>
+  </div>
+</template>
---
+++ src/views/TaskManager.vue
@@ -0,0 +1,64 @@
+<template>
+  <v-list
+      subheader
+      flat
+    >
+    <v-list-item-group
+        v-for="task in tasks"
+        :key="task.id"
+        multiple      
+      >
+        <v-list-item
+          @click="doneTask(task.id)"
+          :class="{'cyan lighten-5' : task.done}"
+        >
+          <template v-slot:default>
+            <v-list-item-action>
+              <v-checkbox
+                :input-value="task.done"
+                color="primary"
+              ></v-checkbox>
+            </v-list-item-action>
+
+            <v-list-item-content
+              :class="{'text-decoration-line-through' : task.done}"
+            >
+              <v-list-item-title>{{ task.title }}</v-list-item-title>
+            </v-list-item-content>
+            
+          </template>
+        </v-list-item>
+        <v-divider></v-divider>
+      </v-list-item-group>
+    </v-list>
+</template>
+
+<script>
+  export default {
+    data: () => ({
+      tasks: [
+        {
+        id: 1, 
+        title: 'Wake Up!',
+        done: false
+        },
+        {
+        id: 2, 
+        title: 'Take a Shower!',
+        done: false
+        },
+        {
+        id: 3, 
+        title: 'Eat Breakfast!',
+        done: false
+        }
+      ]
+    }),
+    methods: {
+      doneTask(id){
+        let task = this.tasks.filter(task => task.id === id)[0]
+        task.done = !task.done
+      }
+    }
+  }
+</script>
