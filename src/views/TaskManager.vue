3f6e91b4f12417e80bd05e5a7a168f55d565a14e
Fernando Goulart <fernandogmoura@protonmail.com>
2022-06-14 14:46:39 -0300

    Add task and delete task

--- src/views/TaskManager.vue
+++ src/views/TaskManager.vue
@@ -1,41 +1,79 @@
 <template>
-  <v-list
-      subheader
-      flat
-    >
-    <v-list-item-group
-        v-for="task in tasks"
-        :key="task.id"
-        multiple      
-      >
-        <v-list-item
-          @click="doneTask(task.id)"
-          :class="{'cyan lighten-5' : task.done}"
-        >
-          <template v-slot:default>
-            <v-list-item-action>
-              <v-checkbox
-                :input-value="task.done"
-                color="primary"
-              ></v-checkbox>
-            </v-list-item-action>
+  
+  <header>
+    <v-text-field
+      v-model="newTaskTitle"
+      @click:append="addTask()"
+      @keyup.enter="addTask()"
+      class="pa-3 pb-0"
+      clearable
+      hide-details
+      outlined
+      label="New Task"
+      append-icon="mdi-plus"
+    ></v-text-field>
 
-            <v-list-item-content
-              :class="{'text-decoration-line-through' : task.done}"
-            >
-              <v-list-item-title>{{ task.title }}</v-list-item-title>
-            </v-list-item-content>
-            
-          </template>
-        </v-list-item>
-        <v-divider></v-divider>
-      </v-list-item-group>
-    </v-list>
+    <v-list
+        v-if="tasks.length"
+        subheader
+        flat
+      >    
+      <v-list-item-group
+          v-for="task in tasks"
+          :key="task.id"
+          multiple      
+        >
+          <v-list-item
+            @click="doneTask(task.id)"
+            :class="{'cyan lighten-5' : task.done}"
+          >
+            <template v-slot:default>
+              <v-list-item-action>
+                <v-checkbox
+                  :input-value="task.done"
+                  color="primary"
+                ></v-checkbox>
+              </v-list-item-action>
+
+              <v-list-item-content
+                :class="{'text-decoration-line-through' : task.done}"
+              >
+                <v-list-item-title>{{ task.title }}</v-list-item-title>
+              </v-list-item-content>
+
+              <v-list-item-action>
+                <v-btn icon
+                  @click.stop="deleteTask(task.id)"
+                >
+                  <v-icon color="black">mdi-delete</v-icon>
+                </v-btn>
+              </v-list-item-action>
+              
+            </template>
+          </v-list-item>
+          <v-divider></v-divider>
+        </v-list-item-group>
+      </v-list>
+      <div v-else
+        class="text-h5 no-tasks"
+      >
+        <v-icon
+          color="teal accent-2"
+          size=150>mdi-check-all</v-icon
+        >
+
+        <div class="teal--text">
+          All Tasks are done!
+        </div>
+
+      </div>
+  </header>
 </template>
 
 <script>
   export default {
     data: () => ({
+      newTaskTitle: '',
       tasks: [
         {
         id: 1, 
@@ -58,7 +96,29 @@
       doneTask(id){
         let task = this.tasks.filter(task => task.id === id)[0]
         task.done = !task.done
+      }, 
+      deleteTask(id){
+        this.tasks = this.tasks.filter(task => task.id !== id)
+      },
+      addTask(){
+        let newTask = {
+          id: Date.now(),
+          title: this.newTaskTitle,
+          done: false
+        }
+        this.tasks.push(newTask)
+        this.newTaskTitle = ''
       }
     }
   }
 </script>
+
+<style scoped>
+.no-tasks {
+  position: absolute;
+  left: 53%;
+  top: 35%;
+  transform: translate(-50%, -50%);
+}
+
+</style>
